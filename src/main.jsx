8f003e637e3dca26606622b25a699d2cdcdb444a
Konkan <muhammedmustafamuhammed831@gmail.com>
2026-05-10 04:15:55 +0300

    Refactor App import and remove Firebase setup
    Updated import statement for App component and removed Firebase initialization code.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCG4cUcG04E1f2moCnmEwG1UiUwHdHuUrs",
-  authDomain: "konkan-fc43e.firebaseapp.com",
-  projectId: "konkan-fc43e",
-  storageBucket: "konkan-fc43e.firebasestorage.app",
-  messagingSenderId: "707049357885",
-  appId: "1:707049357885:web:f0dab6834d2c4600846054",
-  measurementId: "G-E0Y2RRPVKX"
-};
-
-initializeApp(firebaseConfig);
-
-console.log("Firebase Connected");
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
